65525a9a7066b5b48849a356b65586d1179ba1e9
naeonDevelopment <theodor@innovo.global>
2025-02-28 07:38:45 +0200

    Add main App component with routing

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Navbar } from './components/layout/Navbar';
+import { Footer } from './components/layout/Footer';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import DashboardPage from './pages/DashboardPage';
+import NotFoundPage from './pages/NotFoundPage';
+import { AuthProvider } from './context/AuthContext';
+
+const App: React.FC = () => {
+  return (
+    <AuthProvider>
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/dashboard" element={<DashboardPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </AuthProvider>
+  );
+};
+
+export default App;
